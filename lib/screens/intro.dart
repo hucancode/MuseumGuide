f8087d4f27177e2e8096454e8a6d293f4b023f55
Nguyen Huu <nguyenhuu4242@gmail.com>
2021-03-19 16:18:11 +0900

    fix intro screen

--- lib/screens/intro.dart
+++ lib/screens/intro.dart
@@ -26,6 +26,7 @@ class IntroState extends State<IntroPage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            SizedBox(height: 50),
             Text(
               'Museum Guide',
               style: Theme.of(context).
@@ -33,16 +34,28 @@ class IntroState extends State<IntroPage> {
                 headline1?.
                 copyWith(color: Colors.white),
             ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 80, vertical: 20),
+              child: Text(
+                'Welcome to GC museum. We will be using this app to relive the age of the dinosaurs. ',
+                style: Theme.of(context).
+                  textTheme.
+                  bodyText1?.
+                  copyWith(color: Colors.white),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(height: 30),
             TextButton(
               onPressed: () {
                 Navigator.pushNamed(context, "/menu");
               },
               child: Text(
-                'Start!',
+                'Tap here to begin!',
                 style: Theme.of(context).
                   textTheme.
                   headline2?.
-                  copyWith(color: Colors.blueAccent),
+                  copyWith(color: Colors.greenAccent),
               ),
             ),
           ],
