ffd681a9bdba320495b10a4a241288f2ec129a87
Nguyen Huu <nguyenhuu4242@gmail.com>
2021-03-19 13:37:13 +0900

    intro screen

--- lib/screens/intro.dart
+++ lib/screens/intro.dart
@@ -11,22 +11,43 @@ class IntroState extends State<IntroPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0xff53576b),
+        image: DecorationImage(
+          image: AssetImage('assets/intro_illust.png'),
+          alignment: Alignment.topCenter,
+          fit: BoxFit.contain,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Intro screen here',
+              'Museum Guide',
+              style: Theme.of(context).
+                textTheme.
+                headline1?.
+                copyWith(color: Colors.white),
             ),
             TextButton(
               onPressed: () {
                 Navigator.pushNamed(context, "/menu");
               },
-              child: Text('Start!'),
+              child: Text(
+                'Start!',
+                style: Theme.of(context).
+                  textTheme.
+                  headline2?.
+                  copyWith(color: Colors.blueAccent),
+              ),
             ),
           ],
         ),
+      ),
       ),
     );
   }
